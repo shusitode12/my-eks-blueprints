57dfa7440e391b93a8ad60a4d9e6cf0672f93eb4
shusitode12 <137909941+shusitode12@users.noreply.github.com>
2023-11-20 15:26:53 -0600

    Update my-eks-blueprints.ts

--- bin/my-eks-blueprints.ts
+++ bin/my-eks-blueprints.ts
@@ -1,11 +1,12 @@
 // bin/my-eks-blueprints.ts
 import * as cdk from 'aws-cdk-lib';
-import ClusterConstruct from '../lib/my-eks-blueprints-stack';
-
+import ClusterConstruct  from '../lib/my-eks-blueprints-stack';
+//import ClusterConstructv2  from '../lib/my-eks-blueprints-stack-v2';
 
 const app = new cdk.App();
 const account = process.env.CDK_DEFAULT_ACCOUNT!;
 const region = process.env.CDK_DEFAULT_REGION;
 const env = { account, region }
 
-new ClusterConstruct(app, 'cluster', { env });
+new ClusterConstruct(app, 'clusterv2', { env });
+//new ClusterConstructv2(app, 'cluster-v2', { env });
